b21db220d0a52c0fcb8e0ea7e3c09c3733519b4f
William Senkondo <wseko@win.su.se>
2018-11-15 10:33:47 +0100

    Version 0.1.1

--- R/hello.R
+++ R/hello.R
@@ -1,18 +1,12 @@
-# Hello, world!
-#
-# This is an example function named 'hello' 
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Build and Reload Package:  'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
+#' This says hello to any person!
+#'
+#' Here jkh
+#'
+#' @param name A string of characters. Preferentially a name of a person. (Default 'William')
+#' @return Prints Hello name to screen
+#' @examples hello('Malie')
+#' @export
 
-hello <- function() {
-  print("Hello, world!")
+hello <- function(name= 'William') {
+  print(paste("Hello", name))
 }
